dd6de1b94fcf027268cfbb233c4509da142b83ef
Madreyv <madreyv@gmail.com>
2021-11-10 15:37:10 -0300

    Adcionado o layout do projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,184 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Body = styled.div`
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  border: 1px solid red;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
+
+const FilterArea = styled.section`
+  border: 1px solid black;
+  height: 90vh;
+  width:25%;
+  padding: 1%;
+  h3{
+    margin-bottom: 10px;
+  }
+`
+
+const AreaProdutos = styled.section`
+  border: 1px solid green;
+  width: 50%;
+  padding: 1%;
+  height: 90vh;
+`
+
+const AreaCarrinho = styled.section`
+  border: 1px solid yellow;
+  padding: 1%;
+  height: 90vh;
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  h3{
+    margin-bottom: 10px;
+  }
+`
+const ProdutoCarrinho = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`
+
+const HeadProdutos = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const AreaCardProdutos = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+`
+const CardProdutos = styled.div`
+  border: 1px solid black;
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-top: 5%;
+  
+  img{
+    height: 60%;
+  }
+
+  div{
+    display: flex;
+    flex-direction: column;
+    padding: 10%;
+    p{
+        margin: 5px 0;
+    }
+  }
+
+  button{
+    width: 70%;
+    margin: 0 auto;
+  }
+
+`
+
+class App extends React.Component {
+ 
+  render() {
+
+    return (
+      <Body>
+        <FilterArea>
+          <h3>Filtros</h3>
+          <div>
+            <label>Valor Mínimo:</label>
+            <input type="number"/>
+          </div>
+          <div>
+            <label>Valor Máximo:</label>
+            <input type="number"/>
+          </div>
+          <div>
+            <label>Buscar por Nome:</label>
+            <input type="text" placeholder="Produtos"/>
+          </div>
+        </FilterArea>
+        <AreaProdutos>
+          <HeadProdutos>
+            <p>Quantidade de produtos: <b>4</b></p>
+            <div>
+              <label>Ordenação:</label>
+              <select>
+                <option value="">Nenhum</option>
+                <option value="Crescente">Crescente</option>
+                <option value="Decrescente">Decrescente</option>
+              </select>
+            </div>
+          </HeadProdutos>
+          <AreaCardProdutos>
+            <CardProdutos>
+              <img src="https://picsum.photos/400/400?a=2"/>
+              <div>
+                <h5>Titulo</h5>
+                <p>R$50,00</p>
+                <button>Adcionar Ao Carrinho</button>
+              </div>
+            </CardProdutos>
+            <CardProdutos>
+              <img src="https://picsum.photos/400/400?a=2"/>
+              <div>
+                <h5>Titulo</h5>
+                <p>R$50,00</p>
+                <button>Adcionar Ao Carrinho</button>
+              </div>
+            </CardProdutos>
+            <CardProdutos>
+              <img src="https://picsum.photos/400/400?a=2"/>
+              <div>
+                <h5>Titulo</h5>
+                <p>R$50,00</p>
+                <button>Adcionar Ao Carrinho</button>
+              </div>
+            </CardProdutos>
+            <CardProdutos>
+              <img src="https://picsum.photos/400/400?a=2"/>
+              <div>
+                <h5>Titulo</h5>
+                <p>R$50,00</p>
+                <button>Adcionar Ao Carrinho</button>
+              </div>
+            </CardProdutos>
+          </AreaCardProdutos>
+        </AreaProdutos>
+        <AreaCarrinho>
+          <h3>Carrinho:</h3>
+          <ProdutoCarrinho>
+            <p>2x</p>
+            <p>Produto Um</p>
+            <button>Remover</button>
+          </ProdutoCarrinho>
+          <ProdutoCarrinho>
+            <p>1x</p>
+            <p>Produto Dois</p>
+            <button>Remover</button>
+          </ProdutoCarrinho>
+          <p>Valor total: <b>R$100,00</b></p>
+        </AreaCarrinho>
+      </Body>
+    );
+  }
 }
 
 export default App;

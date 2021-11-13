0c50f8aeaef83a957c99359c36725897e3cf6c30
João Paulo <joaop98.junior@gmail.com>
2021-11-12 22:01:10 -0300

    Imagens dos Produtos(Trajes Espaciais)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Carrinho from "./Components/Carrinho/Carrinho";
 import Inputs from "./Components/Inputs/Inputs";
+import Produtos from "./Components/Produtos/Produtos";
+
 import App extends React.Component from "./Imagens-Produtos"
+
 
 const Body = styled.div`
   * {
@@ -16,6 +20,18 @@ const Body = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`
+const Header = styled.header`
+  
+`
+=======
+
 const FilterArea = styled.section`
   border: 1px solid black;
   height: 90vh;
@@ -25,6 +41,7 @@ const FilterArea = styled.section`
     margin-bottom: 10px;
   }
 `;
+
 const AreaProdutos = styled.section`
   border: 1px solid green;
   width: 50%;
@@ -43,14 +60,6 @@ const AreaCarrinho = styled.section`
     margin-bottom: 10px;
   }
 `;
-const ProdutoCarrinho = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 10px;
-`;
-
 const HeadProdutos = styled.div`
   display: flex;
   flex-direction: row;
@@ -84,46 +93,46 @@ const CardProdutos = styled.div`
     margin: 0 auto;
   }
 `;
+ }
 class App extends React.Component {
   state = {
     produtos: [
       {
         id: 1,
-        imagem: "https://static.netshoes.com.br/produtos/camisa-nike-park-dri-fit-masculina/26/HZM-6253-026/HZM-6253-026_zoom1.jpg?ts=1620763234&",
-        nome: "Produto Um",
-        preco: "50,00",
+        imagem: "http://www.nasa.gov/sites/default/files/images/275039main_PLSS-OV_226x289.jpg",
+        nome: "Suporte primário do sistema:PLS",
+        preco: "41.000,00",
       },
       {
         id: Math.floor(Date.now() * Math.random()).toString(36),
-        imagem: "https://static.netshoes.com.br/produtos/jaqueta-puffer-nike-sportswear-feminina/26/HZM-4718-026/HZM-4718-026_zoom1.jpg?ts=1611083384&",
-        nome: "Produto Dois",
-        preco: "510,00",
+        imagem: "http://www.nasa.gov/sites/default/files/images/275044main_upper_226x170.jpg",
+        nome: "Parte superior do tronco",
+        preco: "51.000,00",
       },
       {
         id: Math.floor(Date.now() * Math.random()).toString(36),
-        imagem: "https://static.netshoes.com.br/produtos/camiseta-lacoste-regular-fit/69/D66-6570-C69/D66-6570-C69_zoom1.jpg?ts=1615818967&",
-        nome: "Produto Tres",
-        preco: "150,00",
+        imagem: "http://www.nasa.gov/sites/default/files/images/275034main_lower_226x413.jpg",
+        nome: "Tronco inferior ",
+        preco: "35.000,00",
       },
       {
         id: Math.floor(Date.now() * Math.random()).toString(36),
-        imagem: "https://static.netshoes.com.br/produtos/calca-jeans-skinny-calvin-klein-feminina/62/D70-6062-162/D70-6062-162_zoom1.jpg?ts=1626174075&",
-        nome: "Produto Quatro",
-        preco: "250,00",
+        imagem: "http://www.nasa.gov/sites/default/files/images/275028main_helm_226x189.jpg",
+        nome: "Capacete",
+        preco: "60.000,00",
       },
       {
         id: Math.floor(Date.now() * Math.random()).toString(36),
-        imagem: "https://static.netshoes.com.br/produtos/meia-sem-cano-branca-pacote-c-1-par/14/U07-0032-014/U07-0032-014_zoom1.jpg?ts=1611689738&",
-        nome: "Produto Cinco",
-        preco: "5,00",
+        imagem: "http://www.nasa.gov/sites/default/files/images/275021main_cca_226x170.jpg",
+        nome: "Comunicações-CCA",
+        preco: "15.000,00",
       },
       {
         id: Math.floor(Date.now() * Math.random()).toString(36),
-        imagem: "https://static.netshoes.com.br/produtos/jaqueta-capuz-essentials-insulated-adidas/60/NQQ-7493-060/NQQ-7493-060_zoom1.jpg?ts=1618446340&",
-        nome: "Produto Seis",
-        preco: "588,00",
+        imagem: "http://www.nasa.gov/sites/default/files/images/275032main_lcvg-front_226x301.jpg",
+        nome: "Roupa de Refrigeração Líquida e Ventilação",
+        preco: "25.000,00",
       },
-
     ],
     quantidadeProdutos : null,
     bucarPorNome: "Produto1",
@@ -136,19 +145,29 @@ class App extends React.Component {
         quantidade:1,
         produto:{
           id: 1,
-          imagem: "https://picsum.photos/400/400?a=2",
-          nome: "Produto Cinco",
-          preco: "5,00",
+          imagem: "http://www.nasa.gov/sites/default/files/images/275021main_cca_226x170.jpg",
+          nome: "Comunicações-CCA",
+          preco: "15.00,00",
         }
       }
     ]
   };
 
 
+  componentDidUpdate(){
+    localStorage.setItem("carrinho", JSON.stringify(this.state.carrinho))
+  }
 
 
 
 
+  componentDidMount(){
+    let carrinhoPersistido = localStorage.getItem("carrinho")
+    carrinhoPersistido = JSON.parse(carrinhoPersistido)
+    this.setState({
+      carrinho:carrinhoPersistido || []
+    })
+  }
 
   onChangeBucarPorNome = (event) => {
     this.setState({ bucarPorNome: event.target.value });
@@ -159,13 +178,11 @@ class App extends React.Component {
   onChangeValorMaximo = (event) => {
     this.setState({ valorMaximo: event.target.value });
   };
-
   filtrarProdutos = () =>{
     let produtos = this.state.produtos
     let maiorValor = parseFloat(this.state.valorMaximo) || 1000000
     let menorValor = parseFloat(this.state.valorMinimo) || 0
     let nomeProduto = this.state.bucarPorNome
-
     if(nomeProduto === null){
       produtos = produtos.filter((produto) =>{
         return parseFloat(produto.preco) < maiorValor && parseFloat(produto.preco) > menorValor
@@ -175,14 +192,13 @@ class App extends React.Component {
         return parseFloat(produto.preco) < maiorValor && parseFloat(produto.preco) > menorValor && produto.nome.includes(nomeProduto)
       })
     }
-
     return produtos
   }
-
   onChangeOrdem = (e) => {
     this.setState({
       ordem: e.target.value
     }, () => console.log(this.state.ordem))
+    })
   }
 
   carregarProdutos = () => {
@@ -196,7 +212,6 @@ class App extends React.Component {
         </>
       );
     }
-
     //coloca os produtos de acordo com a orde
     if(this.state.ordem != null ){
       if(this.state.ordem == "Crescente"){
@@ -209,6 +224,7 @@ class App extends React.Component {
         })
       }
     }
+
 
     //cria o JFX dos produtos
     return produtos.map((produto) => {
@@ -223,16 +239,17 @@ class App extends React.Component {
         </CardProdutos>
       );
     });
+
   }
+
+    return produtos
+  };
+
 
   adicionaCarrinho = (e) => {
     let produtoSelecionado = this.state.produtos.filter((produto) =>{
       return produto.id == e.target.value
     })
-
-
-
-
     let controle = 0;
     let carrinhoCarregado =  this.state.carrinho.map((itemCarrinho)=>{
       if(itemCarrinho.produto.id == e.target.value){
@@ -247,18 +264,71 @@ class App extends React.Component {
           quantidade: 1, 
           produto : produtoSelecionado[0]
         }]
-        
+
       },() => console.log(this.state.carrinho))
+      })
     }else{
       this.setState({
         carrinho:carrinhoCarregado 
       }, () => console.log(this.state.carrinho))
+      })
     } 
   };
 
   carrinhoProduto = () => {
     return this.state.carrinho.map((carrinho)=>{
+      return (
+        <Carrinho 
+          produto={carrinho}
+          funcao={this.removerProdutoCarrinho}
+        />
+      )
+    })
+  }
 
+  removerProdutoCarrinho = (e) =>{
+    let produtoSelecionado = this.state.produtos.filter((produto) =>{
+      return produto.id == e.target.value
+    })
+    let controle = 0;
+    let produtosCarrinho = this.state.carrinho.map((item) => {
+      if(item.produto.id == e.target.value && item.quantidade > 1){
+        controle++
+        item.quantidade--
+      }
+
+    let carrinho = this.state.carrinho.filter((item) => {
+      return item.produto.id != e.target.value
+      return item
+    })
+
+    this.setState({
+      carrinho : [...carrinho]
+    })
+    if(controle == 0){
+      let carrinho = this.state.carrinho.filter((item) => {
+        return item.produto.id != e.target.value
+      })
+      this.setState({
+        carrinho : [...carrinho]
+      })
+    }else{
+      this.setState({
+        carrinho : produtosCarrinho
+      })
+    }
+
+  }
+
+  valorCarrinho = () => {
+   let valorTotal = 0 
+    this.state.carrinho.map((item) =>{
+      valorTotal = (parseFloat(item.produto.preco) * parseFloat(item.quantidade)) + valorTotal
+    })
+    return valorTotal 
+  }
+  carrinhoProduto = () => {
+    return this.state.carrinho.map((carrinho)=>{
       return (
         <ProdutoCarrinho>
             <p>{carrinho.quantidade}</p>
@@ -272,42 +342,42 @@ class App extends React.Component {
       )
     })
   }
-
   removerProdutoCarrinho = (e) =>{
     let produtoSelecionado = this.state.produtos.filter((produto) =>{
       return produto.id == e.target.value
     })
-
     let carrinho = this.state.carrinho.filter((item) => {
       return item.produto.id != e.target.value
     })
-
     this.setState({
       carrinho : [...carrinho]
     })
   }
-
   valorCarrinho = () => {
    let valorTotal = 0 
     let testeValor = this.state.carrinho.map((item) =>{
     this.state.carrinho.map((item) =>{
       valorTotal = (parseFloat(item.produto.preco) * parseFloat(item.quantidade)) + valorTotal
-
     })
     return valorTotal 
-
   }
 
 
-    render() 
+  
+
+  render() {
+
     let produtosCarregados = this.carregarProdutos() 
+    render() { 
     let carrinhoProduto = this.carrinhoProduto()
     this.valorCarrinho()
     return (
       <Body>
         <FilterArea>
           <h3>Filtros</h3>
+
           <div>
+
           {/* <Filtro
           /> */}
           <Inputs
@@ -335,6 +405,17 @@ class App extends React.Component {
               value={this.state.valorMinimo}
               type="number"
               onChange={this.onChangeValorMinimo}
+      <>
+        <EstiloGlobal/>
+        <header>Header</header>
+        <Body>
+          <FilterArea>
+            <h3>Filtros</h3>
+            <Inputs
+              titulo="Valor Mínimo:"
+              valor={this.state.valorMinimo}
+              tipo="number"
+              funcao={this.onChangeValorMinimo}
             />
           </div>
           <div>
@@ -343,6 +424,11 @@ class App extends React.Component {
               value={this.state.valorMaximo}
               type="number"
               onChange={this.onChangeValorMaximo}
+            <Inputs
+              titulo="Valor Máximo:"
+              valor={this.state.valorMaximo}
+              tipo="number"
+              funcao={this.onChangeValorMaximo}
             />
           </div>
           <div>
@@ -354,18 +440,37 @@ class App extends React.Component {
               type="text"
               placeholder="Produtos"
               onChange={this.onChangeBucarPorNome}
+            <Inputs
+              titulo="Buscar por Nome:"
+              valor={this.state.bucarPorNome}
+              tipo="text"
+              funcao={this.onChangeBucarPorNome}
+              place="Camisa, Calça"
             />
           </div>
           </div> */}
         </FilterArea>
         <AreaProdutos>
           <HeadProdutos>
+          </FilterArea>
+          <Produtos
+            produtos={this.carregarProdutos()}
+            funcaoOrdem={this.onChangeOrdem}
+            funcaoAdcionarCarrinho={this.adicionaCarrinho}
+          />
+          <AreaCarrinho>
+            <h3>Carrinho:</h3>
+            {carrinhoProduto}
+
             <p>
               Quantidade de produtos: <b>{produtosCarregados.length}</b>
+              Valor total: <b>R$: {this.valorCarrinho()}</b>
             </p>
             <div>
               <label>Ordenação:</label>
+
               <select>
+
               <select onClick={this.onChangeOrdem}>
                 <option value="">Nenhum</option>
                 <option value="Crescente">Crescente</option>
@@ -380,19 +485,21 @@ class App extends React.Component {
         <AreaCarrinho>
           <h3>Carrinho:</h3>
           {carrinhoProduto}
-          
+
           <p>
             Valor total: <b>{this.valorCarrinho()}</b>
           </p>
         </AreaCarrinho>
 
-       
 
-          
+
+
 
       </Body>
+          </AreaCarrinho>
+        </Body>
+      </>
     );
   }
 }
 export default App;
-  
